07c018d9ecfb85678da71f0fa83fd2d7a8f2abf2
wjh <3535025404@qq.com>
2022-08-24 23:31:00 +0800

    修改log4js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  * @version: 
  * @Author: 王家豪
  * @Date: 2022-08-21 23:34:40
- * @LastEditors: 王家豪
- * @LastEditTime: 2022-08-22 18:54:57
+ * @LastEditors: 937bb
+ * @LastEditTime: 2022-08-24 23:29:51
  */
 const port = 443;
 const fs = require('fs');
@@ -17,17 +17,27 @@ const {
   getIPv4
 } = require('./utils/getIPv4')
 let ipv4s = getIPv4()
+const {
+  debugLog,
+  log,
+  logFunction
+} = require('./utils/log4js.js')
 
 
 
 
 app.all('*', function (req, res, next) {
+  // res.send(req)
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   if (req.method == 'OPTIONS') {
     res.send(200);
   } else {
+ 
+    logFunction('http','all',{method:req.method,url:req.url})
+    res.send(req.method);
+
     next();
   }
 });
---
+++ utils/log4js.js
@@ -0,0 +1,115 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 937bb
+ * @Date: 2022-08-24 22:14:10
+ * @LastEditors: 937bb
+ * @LastEditTime: 2022-08-24 23:28:35
+ */
+const log4js = require('log4js');
+
+log4js.configure({
+  replaceConsole: true,
+  appenders: {
+    out: {
+      type: 'console'
+    }, //控制台输出
+    all: {
+      type: 'dateFile',
+      filename: 'logs/all_logs/all',
+      pattern: 'yyyy-MM-dd.log',
+      encoding: 'utf8',
+      alwaysIncludePattern: true,
+      // daysToKeep: 10
+    },
+    debug: { //debug日志
+      type: 'dateFile',
+      filename: 'logs/debug_logs/debug', // 首先手动建好目录，写入日志文件的路径
+      //maxLogSize: 1024,
+      // 只在 type: 'file' 中才支持
+      // 指定pattern后无限备份,pattern精确到ss(秒)就是一秒一个文件,精确到mm(分)就是一分一个文件,hh(小时),dd(天),MM(月),yyyy(年)
+      pattern: 'yyyy-MM-dd.log',
+      encoding: 'utf-8', //文件的编码
+      alwaysIncludePattern: true, // 不指定pattern时若为true会使用 默认值'.yyyy-MM-dd'
+      // daysToKeep: 10, //时间文件 保存多少天，以前的log将被删除
+      //compress : true,//（默认为false） - 在滚动期间压缩备份文件（备份文件将具有.gz扩展名）
+    },
+    err: { //err日志
+      type: 'dateFile',
+      filename: 'logs/error_logs/err',
+      pattern: 'yyyy-MM-dd.log',
+      alwaysIncludePattern: true
+    },
+    info: { //info日志
+      type: 'dateFile',
+      filename: 'logs/info_logs/info',
+      pattern: 'yyyy-MM-dd.log',
+      alwaysIncludePattern: true
+    },
+    fatal: { //fatal日志
+      type: 'dateFile',
+      filename: 'logs/fatal_logs/fatal',
+      pattern: 'yyyy-MM-dd.log',
+      alwaysIncludePattern: true
+    }
+  },
+  categories: {
+    //appenders:采用的appender,取appenders项,level:设置级别
+    debug: {
+      appenders: ['out', 'debug', 'all'],
+      level: 'debug'
+    },
+    err: {
+      appenders: ['out', 'err', 'all'],
+      level: 'error'
+    },
+    info: {
+      appenders: ['out', 'info', 'all'],
+      level: 'info'
+    },
+    fatal: {
+      appenders: ['out', 'fatal', 'all'],
+      level: 'fatal'
+    },
+    default: {
+      appenders: ['out', 'all'],
+      level: 'all'
+    }
+  }
+})
+
+//debug日志
+let debugLog = log4js.getLogger('debug');
+// debugLog.debug('~~111~~debug log~~111~~');
+
+//info日志
+let infoLog = log4js.getLogger('info');
+// infoLog.info('~~222~~info log~~222~~');
+
+//err日志
+let errorLog = log4js.getLogger('err');
+// errorLog.error('~~333~~err log~~333~~');
+
+//fatal日志
+let fatalLog = log4js.getLogger('fatal');
+// fatalLog.fatal('cuo');
+
+let log = log4js.getLogger();
+const logFunction = (title, fun, msg) => {
+  let log = log4js.getLogger(title)
+  if (fun == 'all') {
+    log.all(msg);
+  }
+}
+// logFunction('111', 'all', 'ccc')
+// console.log(allLog)
+// allLog.all('报错了')
+
+module.exports = {
+  debugLog,
+  infoLog,
+  errorLog,
+  fatalLog,
+  log,
+  logFunction
+}
